bd3c09c20f37c1d70c7aa96c2b93d03f5541a556
unknown <info.mdrakibhassan@gmail.com>
2021-01-07 19:51:35 +0600

    User Registration Page UI done

--- app/src/main/java/com/rakib/passwordmanager/ui/Registration.java
+++ app/src/main/java/com/rakib/passwordmanager/ui/Registration.java
@@ -5,28 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 import com.rakib.passwordmanager.R;
 
-public class MainActivity extends AppCompatActivity {
-
-    TextView regTV;
+public class Registration extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registration);
         getSupportActionBar().hide();
     }
 
     public void userLogin(View view) {
-
-    }
-
-
-    public void userRegistration(View view) {
-        startActivity(new Intent(this,Registration.class));
+        startActivity(new Intent(this, MainActivity.class));
         finish();
     }
 }
